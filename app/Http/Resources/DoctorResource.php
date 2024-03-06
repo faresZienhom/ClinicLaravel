ed295f60037ddbd3ec131e88a13d0648e0136d7a
faresZienhom <fareszienhom@gmail.com>
2024-03-06 13:05:10 +0200

    Clinic project

---
+++ app/Http/Resources/DoctorResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DoctorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+
+            'id'=>$this->id,
+            'name'=>$this->name,
+            'city'=>$this->city,
+           'email'=>$this->email,
+           'major'=>$this->major,
+           'image'=>$this->getMedia('doctor_image'),
+
+        ];
+    }
+}
